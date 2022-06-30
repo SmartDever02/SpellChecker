7c491c96bd2c6c3e9d654a14edd8cff582df9561
SmartDever02 <smart.jamesjin@gmail.com>
2022-06-30 10:32:52 -0400

    compress deleted

--- node-back-end/src/express.js
+++ node-back-end/src/express.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const cors = require('cors');
-const compression = require('compression');
 const bodyParser = require('body-parser');
 
 module.exports = function expressWrap(routes) {
@@ -15,7 +14,6 @@ module.exports = function expressWrap(routes) {
     next();
   });
 
-  app.use(compression());
   app.use(bodyParser.urlencoded({ extended: false }));
   app.use(bodyParser.json());
   app.use(routes);
